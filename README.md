1afce3024b3c4051d3c1b9f9d526220af26bbd5d
cardigan1008 <yunboni@smail.nju.edu.cn>
2024-12-12 19:23:47 +0800

    feat: synthesizer(without renaming)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Cprofiler
-A tool for profiling C program. This is a part of offline mode of our LLM-based compiler testing tool.
+# Csynth
+A tool for profiling C program and then synthesizing mutants based on the profiled program.
+This is a part of our LLM-based compiler testing tool.
 
 ## Usage
 
@@ -19,3 +20,16 @@ This step will add profile information and replace code with the profiled one to
 ```shell
 $ python3 profiler.py --src <source database> --dst <destination database>
 ```
+
+**Step3: Synthesize the profiled program**
+
+This step will produce synthesized programs with the programs in the source database and 
+store the mutants in the destination directory.
+
+```shell
+$ python3 synthesizer.py --dst <destination dir> --db <source database>
+```
+
+## TODO
+
+- [ ] Rename tags before synthesizing mutants
---
+++ synthesizer.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+import string
+import os, re, tempfile, sys, argparse, shutil
+from datetime import datetime
+from copy import copy
+import random
+
+from functioner import FunctionDB
+from profiler import MAX_CONST_CCOMP, Var
+from variable import VarType
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+MAX_CHAIN_NUM = 50 # maximum number of functions for one synthesis chain
+
+class Synthesizer:
+    def __init__(self, func_database:str, prob:int) -> None:
+        assert 0 < prob <= 100
+        self.prob = prob
+        self.functionDB = FunctionDB(func_database)
+    
+    def ignore_typedef(self, _typedef:str) -> bool:
+        ignored_typedef = [
+            "int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t", "uint32_t", "int64_t", "uint64_t", "char"
+        ]
+        for ignored in ignored_typedef:
+            if f'{ignored};' in _typedef:
+                return True
+        return False
+
+    def insert_func_decl(self, func_id_list):
+        # locate the last header include
+        headers = re.findall(r'(#include.*)', self.src_syn)
+        if len(headers) == 0:
+            header_end_loc = 0
+        else:
+            header_end_loc = self.src_syn.index(headers[-1]) + len(headers[-1])
+        # insert the function declaration 
+        for func_id in list(set(func_id_list)):
+            for misc in self.functionDB[func_id].misc:
+                if not self.ignore_typedef(misc):
+                    misc = "\n" + misc + "\n"
+                    self.src_syn = self.src_syn[:header_end_loc] + misc + self.src_syn[header_end_loc:]
+                    header_end_loc += len(misc)
+
+            function_body = self.functionDB[func_id].function_body
+            #FIXME: the added attribute may be incompatible with existing function attributes from the database. Can use this feature again if attributes are removed from the database.
+            # prob_attr = random.randint(0, 100-1)
+            # if prob_attr > 50:
+            #     function_body = "inline __attribute__((always_inline))\n" + function_body
+            function_body = "\n" + function_body + "\n"
+            self.src_syn = self.src_syn[:header_end_loc] + function_body + self.src_syn[header_end_loc:]
+            header_end_loc += len(function_body)
+        
+    def synthesize_input(self, env_vars:list[Var], func_inp_list:list[str], func_inp_types:list[VarType]):
+        """Synthesize input to the target function call with environmental variables"""
+        new_inp_list = []
+        for inp_i in range(len(func_inp_list)):
+            inp_value = int(func_inp_list[inp_i])
+            if len(env_vars) > 0:
+                env = random.choice(env_vars)
+                env_value_cast = VarType.get_ctypes(func_inp_types[inp_i], env.var_value).value
+                if abs(env_value_cast) > MAX_CONST_CCOMP:
+                    new_inp_list.append(f"{inp_value}")
+                else:
+                    new_inp_list.append(f"({VarType.to_str(func_inp_types[inp_i])})({env.var_name})+({inp_value - env_value_cast})")
+            else:
+                new_inp_list.append(f"{inp_value}")
+        return new_inp_list
+
+    def synthesize_output(self, env_vars:list[Var], func_out, func_return_type:VarType):
+        """Synthesize output to make sure the function return a value in a reasonable range"""
+        ret_val_min, ret_val_max = VarType.get_range(func_return_type) # the range of the return value
+        func_return_type = VarType.get_base_type(func_return_type) #FIXME: This is a bug in generating proxy functions where we did not convert the proxy return type. This has been fixed in proxy.py but need to regenerate function datebase to get rid of this work-around
+        func_out = int(func_out)
+        output_str = ""
+        output = func_out
+        # if func_out already exceeds the range limit
+        if not (ret_val_min <= func_out <= ret_val_max):
+            output_str += f'-({func_out})'
+            output = 0
+        for env in env_vars:
+            env_value_cast = VarType.get_ctypes(func_return_type, env.var_value).value
+            if abs(env_value_cast) > MAX_CONST_CCOMP or abs(env_value_cast+output) > MAX_CONST_CCOMP:
+                continue
+            if ret_val_min <= env_value_cast+output <= ret_val_max:
+                output_str += f'+({VarType.to_str(func_return_type)})({env.var_name})'
+                output += env_value_cast
+            else:
+                output_str += f'+(({VarType.to_str(func_return_type)})({env.var_name})-({env_value_cast}))'
+        return output_str, output
+
+    def replace_valuetag_with_func(self, tag_id:int, tgt_func_idx:int):
+        """
+        Replace a ValueTag with the selected function call
+        """
+        # use stable tag_var and env_vars for synthesis
+        stable_env_vars = []
+        if self.tags[tag_id].tag_var.is_stable:
+            stable_env_vars.append(self.tags[tag_id].tag_var)
+        for env in self.tags[tag_id].tag_envs:
+            if env.is_stable:
+                stable_env_vars.append(env)
+
+        # select the first io pair of the tgt_func
+        func_inp_list, func_out = self.functionDB[tgt_func_idx].io_list[0]
+        new_input_str = self.synthesize_input(stable_env_vars, func_inp_list, self.functionDB[tgt_func_idx].args_type)
+        new_output_str, new_output = self.synthesize_output(stable_env_vars, func_out, self.functionDB[tgt_func_idx].return_type)
+        
+        # synthesize func_call for expr, make sure to restore the value of the expr
+        if not self.tags[tag_id].is_statement:
+            func_call = "(({tag_type})({call_name}({input}){output})+{tag_var_value})".format(
+                tag_type=self.tags[tag_id].tag_var.var_type, 
+                call_name=self.functionDB[tgt_func_idx].call_name, 
+                input=", ".join(new_input_str), 
+                output=f"{new_output_str}-({new_output})",
+                tag_var_value=self.tags[tag_id].tag_var.var_name,
+            )
+        # for statement tag, we also want to assign the function call to a stable env variable
+        else:
+            func_call = "({call_name}({input}){output})".format(
+                call_name=self.functionDB[tgt_func_idx].call_name, 
+                input=",".join(new_input_str), 
+                output=new_output_str
+            )
+            restore_env = None
+            if not self.tags[tag_id].tag_var.is_constant:
+                restore_env = self.tags[tag_id].tag_var
+            elif len(stable_env_vars) > 0:
+                restore_env = random.choice(stable_env_vars)
+            if restore_env is not None and not restore_env.is_constant:
+                func_call = f'{restore_env.var_name} = ({restore_env.var_type})({func_call}-({new_output}))+({restore_env.var_value});'
+
+        # insert the function call
+        self.src_syn = self.src_syn.replace(self.tags[tag_id].tag_str, f'/*TAG{tag_id}:STA*/' + func_call + f'/*TAG{tag_id}:END:{self.tags[tag_id].tag_var.var_name}*/')
+
+    def synthesize_one(self, tgt_func_idx:int, used_func:list[int], replaced_tags:dict[int, list[int]]):
+        """
+        Synthesize functions for tags in the target function
+        """
+        tgt_func = self.functionDB[tgt_func_idx]
+        synth_funcs = []
+        for tag_id in tgt_func.alive_tags:
+            if tag_id in replaced_tags[tgt_func_idx] or random.randint(0, 100) > self.prob:
+                continue
+            while True:
+                synth_func_idx = random.choice(0, len(self.functionDB)-1)
+                if self.functionDB[synth_func_idx].has_io and synth_func_idx not in used_func:
+                    break
+            self.replace_valuetag_with_func(tag_id, synth_func_idx)
+            replaced_tags[tgt_func_idx].append(tag_id)
+            synth_funcs.append(synth_func_idx)
+            used_func.append(synth_func_idx)
+        return synth_funcs
+
+    def synthesizer(self, dst_dir:str, num_mutant:int=1, DEBUG:bool=False):
+        """
+        Synthesize a source file by replacing variables/constants with function calls.
+        """
+        # randomly select a seed function
+        while True:
+            seed_func_idx = random.randint(0, len(self.functionDB)-1)
+            if self.functionDB[seed_func_idx].has_io:
+                seed_func = self.functionDB[seed_func_idx]
+                break
+
+        self.DEBUG = DEBUG
+        assert num_mutant >= 1
+
+        succ_file_id = id_generator()
+        src_filename = str((dst_dir / f"{succ_file_id}_seed.c").absolute())
+        with open(src_filename, "w") as f:
+            for misc in seed_func.misc:
+                f.write(misc)
+                f.write("\n")
+            f.write(seed_func.function_body)
+            f.write("\n")
+            f.write("int main() {\n")
+            f.write("{func_name}({func_io});\n".format(func_name=seed_func.call_name, func_io = ','.join(map(str, seed_func.io_list[0][0]))))
+            f.write("return 0;\n")
+            f.write("}\n")
+
+        # backup src file
+        tmp_f = tempfile.NamedTemporaryFile(suffix=".c", delete=False)
+        tmp_f.close
+        shutil.copy(src_filename, tmp_f.name)
+        with open(tmp_f.name, "r") as f:
+            self.src_orig = f.read()
+        os.remove(tmp_f.name)
+
+        # sythesis
+        seed_alive_tags = seed_func.alive_tags  # tags that are not replaced by constants
+        all_syn_files = []
+        if len(seed_alive_tags) == 0:
+            return all_syn_files
+        for num_i in range(num_mutant):
+            if self.DEBUG:
+                print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), ">synthesize mutatant start", num_i, flush=True)
+            self.src_syn = copy(self.src_orig)
+            used_func = [seed_func_idx]
+            replaced_tags = [] # a dict that contains the replaced tags of used functions(ready to be tgts)
+            for _ in range(random.randint(MAX_CHAIN_NUM / 2, MAX_CHAIN_NUM)):
+                tgt_func_idx = random.choice(used_func)
+                # FIXME: we need to change the tag name to be unique with func_name so that we can precisely replace them
+                self.tags = self.functionDB[tgt_func_idx].profile
+                synth_funcs = self.synthesize_one(tgt_func_idx, used_func, replaced_tags)
+                used_func.extend(synth_funcs)
+                self.insert_func_decl(synth_funcs)
+            dst_filename = f'{os.path.splitext(src_filename)[0]}_syn{num_i}.c'
+            with open(dst_filename, "w") as f:
+                f.write(self.src_syn)
+            all_syn_files.append(dst_filename)
+            if self.DEBUG:
+                print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), ">synthesize mutatant end", num_i, flush=True)
+
+        return all_syn_files
+
+
+class SynthesizerError(Exception):
+    pass
+
+
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser(description='Synthesize a new program based on a seed program and a function database.')
+    parser.add_argument('--dst', dest='DST', required=True, help='path to the destination dir.')
+    parser.add_argument('--db', dest='DB', required=True, help='path to the function database json file.')
+    args = parser.parse_args()
+    if not os.path.exists(args.DB):
+        print(f"File {args.DB} does not exist!")
+        parser.print_help()
+        exit(1)
+
+    syner = Synthesizer(args.DB, prob=100)
+    try:
+        all_syn_files = syner.synthesizer(args.DST, num_mutant=1)
+    except SynthesizerError:
+        print("SynthesizerError (OK).")
